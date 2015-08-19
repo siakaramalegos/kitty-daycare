103d55fd1ed9dcf68549aff376a88fc72eefdbe4
Sia <siakaramalegos@gmail.com>
2015-08-19 10:06:21 -0500

    Changed cat avatar to center and crop

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -4,8 +4,13 @@ class Cat < ActiveRecord::Base
 
   validates :name, :owner_id, :breed_id, presence: true
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar,
+    :styles => {
+      :medium => "300x300#",
+      :thumb => "100x100#" },
+    :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_size :avatar, :less_than => 3.megabytes, :unless => Proc.new {|m| m[:avatar_file_name].blank?}
 end
 
 # == Schema Information
